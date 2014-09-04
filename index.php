2464963ad28b3e6a94c3be59172806f89cb7e026
ujjwal <ujjwal@parakhi.com>
2014-09-04 15:56:14 +0545

    added backgrounds to index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	Log In Interface
 	</title>
 	<script src="javascript/on_off.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/button.css">
 
 	<style type="text/css">
 	
@@ -11,7 +12,7 @@
 		div.lnks{cursor:pointer;color:red;font-weight:bold;font-size:20px;font-family:calibri;padding:10px;}
 	</style>
 </head>
- <body style="background-color:#b1cdB1">
+ <body style="background-image:url('images/background2.png')">
 	<div style="width:100%;background-color:none;height:200px;">
 		<div style="width:42%;height:200px;float:left;background-color:none;"></div>
 		
@@ -25,37 +26,38 @@
 	<h1 align="center"> Nepal Government</h1>
 	<h2 align="center">Log-in Interface</h2>
 		
-	<div class="main-form-body">
-	<div class="left-space" style="width:40%;height:400px;float:left;color:red;background-color:none;">
+	
+	<div class="left-space" style="width:40%;height:250px;float:left;color:red;background-color:none;">
 	</div>
 	
-	<div class="right-space" style="width:38%;height:400px;float:right;color:red;background-color:none;">
+	<div class="right-space" style="width:38%;height:250px;float:right;color:red;background-color:none;">
 	</div>
 	
 	
 
+
 	
 	
-	<div name="login" id="login" width="20%" height="3900px" style="float:center;background-color:#12A1A1;border-radius:12px;display:block;">
-		<h3 align="center">Employee</h3>
-		<form method="POST" action="login_verification.php">
-			<table>
-				<tr>
-					<td>Login</td>
-					<td><input type="text" name="user"></input></td>
-				</tr>
-				<tr>
-					<td>Password</td>
-					<td><input type="password" name="pass"></input></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type="submit" value="login"></input></td>
-				</tr>
-			</table>
-			
-		</form>
-	</div>
+		<div name="login" id="login" style="float:center;border-radius:12px;display:block;">
+
+			<form method="POST" action="login_verification.php">
+				<table style="background-image:url('images/background.png');border-radius:12px;float:center">
+					<tr>
+						<td><b>Login</b></td>
+						<td><input type="text" name="user"></input></td>
+					</tr>
+					<tr>
+						<td><b>Password</b></td>
+						<td><input type="password" name="pass"></input></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" class="button" value="    LOGIN    "></input></td>
+					</tr>
+				</table>
+				
+			</form>
+		</div>
 	
 
 </body>
